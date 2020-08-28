36dcce0924daa70ce263f1d4b6be98e3cdb3bbb7
Sandilya Jandhyala <jysandilya@gmail.com>
2020-08-28 11:35:01 +0530

    Add frontend tests for the initialize event
    Also deletes some dead code.
    Adds xmlhttprequest so that the tests work in a
    node.js environment.

--- src/cljs/chronograph_web/subscriptions.cljs
+++ src/cljs/chronograph_web/subscriptions.cljs
@@ -2,16 +2,6 @@
   (:require [re-frame.core :as rf]))
 
 (rf/reg-sub
-  ::google-client-initialized?
-  (fn [db _]
-    (:google-client-initialized? db)))
-
-(rf/reg-sub
-  ::signed-in?
-  (fn [db _]
-    (:signed-in? db)))
-
-(rf/reg-sub
   ::signin-state
   (fn [db _]
     (get-in db [:user :signin-state])))
