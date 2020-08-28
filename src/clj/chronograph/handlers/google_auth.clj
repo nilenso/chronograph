19dd58cca8aad1b90f011a2b680d45103e29d06a
Sandilya Jandhyala <jysandilya@gmail.com>
2020-08-28 16:42:13 +0530

    Improve error handling in the oauth redirect handler
    - Errors from Google's token endpoint are handled.
    - Unexpected errors are caught and rendered via redirect.

--- src/clj/chronograph/handlers/google_auth.clj
+++ src/clj/chronograph/handlers/google_auth.clj
@@ -6,7 +6,8 @@
             [chronograph.db.users :as users-db]
             [org.httpkit.client :as http]
             [mount.core :refer [defstate]]
-            [cheshire.core :as json])
+            [cheshire.core :as json]
+            [taoensso.timbre :as log])
   (:import (com.google.api.client.googleapis.auth.oauth2 GoogleIdTokenVerifier$Builder GoogleIdTokenVerifier)
            (com.google.api.client.googleapis.javanet GoogleNetHttpTransport)
            (com.google.api.client.json.jackson2 JacksonFactory)))
@@ -26,10 +27,10 @@
            (into {})))
 
 (defn- redirect-uri [{:keys [redirect-uri
-                            client-id
-                            response-type
-                            scope
-                            login-endpoint] :as oauth-config}]
+                             client-id
+                             response-type
+                             scope
+                             login-endpoint] :as oauth-config}]
   (format "%s?redirect_uri=%s&client_id=%s&scope=%s&response_type=%s"
           login-endpoint
           (codec/url-encode redirect-uri)
@@ -47,26 +48,38 @@
                              (codec/url-encode "auth-error")
                              (codec/url-encode error))))
 
+(defn- fetch-id-token [auth-code]
+  (let [{:keys [token-endpoint
+                client-id
+                client-secret
+                redirect-uri]} (get-in config/config [:oauth :google])
+        {:keys [status body]} @(http/post token-endpoint
+                                          {:form-params {"client_id"     client-id
+                                                         "client_secret" client-secret
+                                                         "code"          auth-code
+                                                         "grant_type"    "authorization_code"
+                                                         "redirect_uri"  redirect-uri}})]
+    (if-not (= 200 status)
+      (throw (ex-info "Received an unexpected status code from Google's token endpoint"
+                      {:status status
+                       :body   body}))
+      (-> body
+          json/parse-string
+          (get "id_token")))))
+
 (defn oauth2-redirect-handler [{:keys [params] :as request}]
-  (if (:error params)
-    (redirect-with-error (:error params))
-    (let [{:keys [token-endpoint
-                  client-id
-                  client-secret
-                  redirect-uri]} (get-in config/config [:oauth :google])
-          token-response @(http/post token-endpoint
-                                     {:form-params {"client_id"     client-id
-                                                    "client_secret" client-secret
-                                                    "code"          (:code params)
-                                                    "grant_type"    "authorization_code"
-                                                    "redirect_uri"  redirect-uri}})
-          id-token       (some-> token-response
-                                 :body
-                                 json/parse-string
-                                 (get "id_token"))
-          {:strs [name sub email email_verified picture]} (token->credentials id-token)]
-      (if-not email_verified
-        (redirect-with-error "email-not-verified")
-        (let [{:keys [id name email photo-url]} (users-db/find-or-create-google-user! sub name email picture)]
-          (-> (response/redirect "/")
-              (auth/set-auth-cookie id email name photo-url)))))))
+  (try
+    (if (:error params)
+      (redirect-with-error (:error params))
+      (let [id-token (fetch-id-token (:code params))
+            {:strs [name sub email email_verified picture]} (token->credentials id-token)]
+        (if-not email_verified
+          (redirect-with-error "email-not-verified")
+          (let [{:keys [id name email photo-url]} (users-db/find-or-create-google-user! sub name email picture)]
+            (-> (response/redirect "/")
+                (auth/set-auth-cookie id email name photo-url))))))
+    (catch Exception e
+      ;; We can't bubble up the exception and rely on our middleware here,
+      ;; because we need to render the error appropriately to the user.
+      (log/error e)
+      (redirect-with-error "unexpected-error"))))
--- test/clj/chronograph/handlers/google_auth_test.clj
+++ test/clj/chronograph/handlers/google_auth_test.clj
@@ -13,8 +13,9 @@
 
 (deftest oauth2-redirect-handler-test
   (testing "when called with a code and no error"
-    (mc/with-mock [http/post                      (delay {:body (-> {"id_token" "fake-token"}
-                                                                    (json/generate-string))})
+    (mc/with-mock [http/post                      (delay {:status 200
+                                                          :body   (-> {"id_token" "fake-token"}
+                                                                      (json/generate-string))})
                    google-auth/token->credentials {"name"           "foobar"
                                                    "sub"            "123456"
                                                    "email"          "foo@bar.com"
@@ -40,8 +41,9 @@
              (get headers "Location")))))
 
   (testing "when the email from the id_token is not verified"
-    (mc/with-mock [http/post                      (delay {:body (-> {"id_token" "fake-token"}
-                                                                    (json/generate-string))})
+    (mc/with-mock [http/post                      (delay {:status 200
+                                                          :body   (-> {"id_token" "fake-token"}
+                                                                      (json/generate-string))})
                    google-auth/token->credentials {"name"           "foobar"
                                                    "sub"            "123456"
                                                    "email"          "foo@bar.com"
@@ -50,4 +52,20 @@
       (let [{:keys [status headers]} (google-auth/oauth2-redirect-handler {:params {:code "123"}})]
         (is (= 302 status))
         (is (= "/?auth-error=email-not-verified"
+               (get headers "Location"))))))
+
+  (testing "when the token endpoint returns a non-200 status code"
+    (mc/with-mock [http/post (delay {:status 500
+                                     :body   (-> {"error" "big problem"}
+                                                 (json/generate-string))})]
+      (let [{:keys [status headers]} (google-auth/oauth2-redirect-handler {:params {:code "123"}})]
+        (is (= 302 status))
+        (is (= "/?auth-error=unexpected-error"
+               (get headers "Location"))))))
+
+  (testing "when the call to the token endpoint throws an exception"
+    (mc/with-mock [http/post (delay (ex-info "Timeout!" {:time :out}))]
+      (let [{:keys [status headers]} (google-auth/oauth2-redirect-handler {:params {:code "123"}})]
+        (is (= 302 status))
+        (is (= "/?auth-error=unexpected-error"
                (get headers "Location")))))))
