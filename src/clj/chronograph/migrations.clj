d6b78f926acae09e281ec2efb4ff5a3aea07b7cf
Ravi Chandra Padmala <neena@nilenso.com>
2020-08-31 16:56:46 +0530

    Move create-migration fn into dev ns

--- src/clj/chronograph/migrations.clj
+++ src/clj/chronograph/migrations.clj
@@ -6,9 +6,6 @@
   {:store :database
    :db    {:connection-uri (:db-connection-string config/config)}})
 
-(defn create-migration [migration-name]
-  (migratus/create (config) migration-name))
-
 (defn migrate []
   (migratus/migrate (config)))
 
