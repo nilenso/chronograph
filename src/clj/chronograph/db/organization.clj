6081225a7bf1c63800da30655416eefe715b505d
Aditya Athalye <aditya@nilenso.com>
2020-09-11 11:37:15 +0530

    Add db fn to find one org based on slug. We use
    slug because it is guaranteed unique, indexed for lookup, lets us avoid
    leaking the table's primary key into the world, and is the human-
    readable reference that we expect clients to use.

--- src/clj/chronograph/db/organization.clj
+++ src/clj/chronograph/db/organization.clj
@@ -1,7 +1,8 @@
 (ns chronograph.db.organization
   (:require [chronograph.db.core :as db]
             [next.jdbc.sql :as sql]
-            [chronograph.utils.time :as time]))
+            [chronograph.utils.time :as time]
+            [next.jdbc :as jdbc]))
 
 (defn create!
   ([organization]
@@ -15,3 +16,20 @@
                    :created-at now
                    :updated-at now}
                   db/sql-opts))))
+
+
+(defn find-one
+  "Fetch the exact match organization for the given slug and user IFF
+  the user belongs to the organization's ACL. Else return nil."
+  ([slug user-id]
+   (find-one db/datasource slug user-id))
+  ([tx slug user-id]
+   (jdbc/execute-one! tx
+                      ["SELECT organizations.*
+                        FROM organizations
+                        INNER JOIN acls ON
+                                   organizations.id = acls.organization_id
+                                   AND organizations.slug = ?
+                                   AND acls.user_id = ?"
+                       slug user-id]
+                      db/sql-opts)))
