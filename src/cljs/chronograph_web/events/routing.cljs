430d484b5450df84e4b4960eda1b6af152da2ef5
Aditya Athalye <aditya@nilenso.com>
2020-09-14 20:53:42 +0530

    [DRAFT] Frontend for the get organization route
    TODO: fix "Not found" ghost when we try to navigate to an organization
    page. e.g. on visiting the /organization/a-wonderful-org page, which
    should fetch info about a-wonderful-org.
    
      - The page renders "Loading..." which is ok,
      - then it briefly displays "Not found"; I think this is while the GET
        request is in progress (and maybe other effects)
      - then on successful fetch, it renders the organization details
    
    TODO: optimize rendering in case we already have :organizations data
    (e.g. just after rendering).

--- src/cljs/chronograph_web/events/routing.cljs
+++ src/cljs/chronograph_web/events/routing.cljs
@@ -1,11 +1,6 @@
 (ns chronograph-web.events.routing
   (:require [re-frame.core :as rf]))
 
-(rf/reg-event-db
-  ::set-page
-  (fn [db [_ route]]
-    (assoc db :page route)))
-
 (rf/reg-event-fx
   ::set-token
   (fn [_ [_ token]]
