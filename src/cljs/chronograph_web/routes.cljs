5dfd43b29cea9a107000e6a2c8d63e964dccc8c6
Akshatha Herle <akshatha@nilenso.com>
2020-09-28 18:56:58 +0530

    Add pending invites route

--- src/cljs/chronograph_web/routes.cljs
+++ src/cljs/chronograph_web/routes.cljs
@@ -6,15 +6,18 @@
             [chronograph-web.pages.landing.views :refer [landing-page]]
             [chronograph-web.pages.create-organization.views :refer [create-organization-page]]
             [chronograph-web.pages.organization.views :refer [organization-page]]
+            [chronograph-web.pages.pending-invites.views :refer [pending-invites-page]]
             [chronograph-web.events.routing :as routing-events]))
 
 (def routes ["/" {"" :root
                   "organization/" {"new" :organization-new
-                                   [:slug] :organization-show}}])
+                                   [:slug] :organization-show}
+                  "pending-invites" :pending-invites}])
 
 (def authenticated-view {:root landing-page
                          :organization-new create-organization-page
-                         :organization-show organization-page})
+                         :organization-show organization-page
+                         :pending-invites pending-invites-page})
 
 (defn set-page! [match]
   (rf/dispatch [::routing-events/pushy-dispatch match]))
