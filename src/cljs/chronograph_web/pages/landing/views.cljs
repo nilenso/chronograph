ca26696e971f5055594e6d7754008cd7822c8059
Akshatha Herle <akshatha@nilenso.com>
2020-09-28 18:48:19 +0530

    Add link to pending invites page on landing page

--- src/cljs/chronograph_web/pages/landing/views.cljs
+++ src/cljs/chronograph_web/pages/landing/views.cljs
@@ -5,6 +5,7 @@
 (defn landing-page [_]
   (let [{:keys [name email photo-url]} @(rf/subscribe [::subs/user-info])]
     [:div
+     [:a {:href "/pending-invites"} "You have pending invites!"]
      [:a {:href "/organization/new"} "New Organization"]
      [:h2 "Welcome!"]
      [:img {:src photo-url}]
