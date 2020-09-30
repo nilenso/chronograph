2fb969da2439564b3b166336eac9f803d4c0e9ea
Akshatha Herle <akshatha@nilenso.com>
2020-09-30 10:24:30 +0530

    Add event handlers to accept and reject invites

--- src/cljs/chronograph_web/pages/pending_invites/events.cljs
+++ src/cljs/chronograph_web/pages/pending_invites/events.cljs
@@ -4,4 +4,14 @@
 (rf/reg-event-db
   ::reject-invite
   (fn [db [_ id]]
-    (update db :invites dissoc id)))
+    (update db :organization-invites
+            (fn [invites]
+              (remove #(= (:id %) id) invites)))))
+
+(rf/reg-event-db
+  ::accept-invite
+  (fn [db [_ id]]
+    (update db :organization-invites
+            (fn [invites]
+              (remove #(= (:id %) id) invites)))))
+
