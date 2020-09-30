716642319d40f1b5ff1ec50897e054d76ba8f2fa
Akshatha Herle <akshatha@nilenso.com>
2020-09-30 10:25:11 +0530

    Register subscription to query invites from app-db

--- src/cljs/chronograph_web/pages/pending_invites/subscriptions.cljs
+++ src/cljs/chronograph_web/pages/pending_invites/subscriptions.cljs
@@ -1,1 +1,7 @@
-(ns chronograph-web.pages.pending-invites.subscriptions)
+(ns chronograph-web.pages.pending-invites.subscriptions
+  (:require [re-frame.core :as rf]))
+
+(rf/reg-sub
+  ::invites
+  (fn [db _]
+    (get db :organization-invites)))
