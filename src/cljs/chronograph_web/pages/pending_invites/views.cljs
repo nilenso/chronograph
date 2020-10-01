76caa4f820fbe7b35fe48138ace7c57e7236fa30
Akshatha Herle <akshatha@nilenso.com>
2020-10-01 12:15:39 +0530

    Dispatch page mounted event for pending invites

--- src/cljs/chronograph_web/pages/pending_invites/views.cljs
+++ src/cljs/chronograph_web/pages/pending_invites/views.cljs
@@ -4,14 +4,16 @@
             [chronograph-web.pages.pending-invites.subscriptions :as invites-subs]))
 
 (defn pending-invites-page [_]
-  (let [invites @(rf/subscribe [::invites-subs/invites])]
-    [:div
-     [:ul
-      (map (fn [{:keys [id name]}]
-             [:li
-              [:p name]
-              [:button {:on-click #(rf/dispatch [::invites-events/reject-invite id])}
-               "reject"]
-              [:button {:on-click #(rf/dispatch [::invites-events/accept-invite id])}
-               "accept"]])
-           invites)]]))
+  (rf/dispatch [::invites-events/page-mounted])
+  (fn [_]
+    (let [invites @(rf/subscribe [::invites-subs/invites])]
+      [:div
+       [:ul
+        (map (fn [{:keys [id name]}]
+               [:li
+                [:p name]
+                [:button {:on-click #(rf/dispatch [::invites-events/reject-invite id])}
+                 "reject"]
+                [:button {:on-click #(rf/dispatch [::invites-events/accept-invite id])}
+                 "accept"]])
+             invites)]])))
--- test/cljs/chronograph/pages/pending_invites/events_test.cljs
+++ test/cljs/chronograph/pages/pending_invites/events_test.cljs
@@ -13,27 +13,36 @@
 (use-fixtures :once fixtures/check-specs)
 
 (deftest pending-invites-page-test
+  (testing "When page is mounted, db should contain invites"
+    (rf-test/run-test-sync
+      (tu/initialize-db!)
+      (tu/set-token "/pending-invites")
+      (rf/dispatch [::invites/page-mounted])
+      (is (= [{:id 1 :slug "slug1" :name "org1"}
+              {:id 2 :slug "slug2" :name "org2"}
+              {:id 3 :slug "slug3" :name "org3"}]
+             @(rf/subscribe [::invites-subs/invites])))))
   (testing "When invite is rejected, it should be removed from db"
     (rf-test/run-test-sync
-     (tu/initialize-db!)
-     (swap! db/app-db assoc :organization-invites {1 {:id 1 :slug "slug1" :name "org1"}
-                                                   2 {:id 2 :slug "slug2" :name "org2"}
-                                                   3 {:id 3 :slug "slug3" :name "org3"}})
-     (rf/dispatch [::invites/reject-invite 2])
-     (is (= [{:id 1 :slug "slug1" :name "org1"}
-             {:id 3 :slug "slug3" :name "org3"}]
-            @(rf/subscribe [::invites-subs/invites])))))
+      (tu/initialize-db!)
+      (swap! db/app-db assoc :organization-invites {1 {:id 1 :slug "slug1" :name "org1"}
+                                                    2 {:id 2 :slug "slug2" :name "org2"}
+                                                    3 {:id 3 :slug "slug3" :name "org3"}})
+      (rf/dispatch [::invites/reject-invite 2])
+      (is (= [{:id 1 :slug "slug1" :name "org1"}
+              {:id 3 :slug "slug3" :name "org3"}]
+             @(rf/subscribe [::invites-subs/invites])))))
 
   (testing "When invite is accepted, it should be removed from db and user is sent to organization detail page"
     (rf-test/run-test-sync
-     (tu/stub-routing)
-     (tu/initialize-db!)
-     (swap! re-frame.db/app-db assoc :organization-invites {1 {:id 1 :slug "slug1" :name "org1"}
-                                                            2 {:id 2 :slug "slug2" :name "org2"}
-                                                            3 {:id 3 :slug "slug3" :name "org3"}})
-     (rf/dispatch [::invites/accept-invite 2])
-     (is (= [{:id 1 :slug "slug1" :name "org1"}
-             {:id 3 :slug "slug3" :name "org3"}]
-            @(rf/subscribe [::invites-subs/invites])))
-     (is (= {:route-params {:slug "slug2"}, :handler :organization-show}
-            @(rf/subscribe [::subs/current-page]))))))
+      (tu/stub-routing)
+      (tu/initialize-db!)
+      (swap! re-frame.db/app-db assoc :organization-invites {1 {:id 1 :slug "slug1" :name "org1"}
+                                                             2 {:id 2 :slug "slug2" :name "org2"}
+                                                             3 {:id 3 :slug "slug3" :name "org3"}})
+      (rf/dispatch [::invites/accept-invite 2])
+      (is (= [{:id 1 :slug "slug1" :name "org1"}
+              {:id 3 :slug "slug3" :name "org3"}]
+             @(rf/subscribe [::invites-subs/invites])))
+      (is (= {:route-params {:slug "slug2"}, :handler :organization-show}
+             @(rf/subscribe [::subs/current-page]))))))
