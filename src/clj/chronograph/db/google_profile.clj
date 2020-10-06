48568a89928b925cf99b00bc571ec1f66ae331ea
Govind krishna Joshi <govind@nilenso.com>
2020-10-06 17:20:44 +0530

    Use db core functions in google profiles

--- src/clj/chronograph/db/google_profile.clj
+++ src/clj/chronograph/db/google_profile.clj
@@ -1,16 +1,7 @@
 (ns chronograph.db.google-profile
-  (:require [next.jdbc.sql :as sql]
-            [chronograph.db.core :as db]
-            [chronograph.utils.time :as time]))
+  (:require [chronograph.db.core :as db]))
 
-(defn create!
-  ([google-id]
-   (create! db/datasource google-id))
-  ([tx google-id]
-   (let [now (time/now)]
-     (sql/insert! tx
-                  :google-profiles
-                  {:google-id  google-id
-                   :created-at now
-                   :updated-at now}
-                  db/sql-opts))))
+(defn create! [tx {:google-profiles/keys [google-id]}]
+  (db/create! :google-profiles
+              tx
+              {:google-id google-id}))
