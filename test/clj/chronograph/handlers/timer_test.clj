481a02a930b280d84e1c0af8275bd3a0be5c487a
Aditya Athalye <aditya@nilenso.com>
2020-10-06 22:26:05 +0530

    Remove atom-based fixture from timer tests to allow
    deftest-level parallelism. Also refactor some of the deftests into more
    readable blocks.

--- test/clj/chronograph/handlers/timer_test.clj
+++ test/clj/chronograph/handlers/timer_test.clj
@@ -8,10 +8,7 @@
             [chronograph.domain.timer :as timer]
             [chronograph.fixtures :as fixtures]))
 
-(def test-context
-  (atom {}))
-
-(defn- setup-org-users-tasks
+(defn- setup-org-users-tasks!
   []
   (let [{user-1-id :users/id}
         (factories/create-user)
@@ -27,93 +24,87 @@
                         :role acl/member})
         {task-id-1 :tasks/id} (factories/create-task organization)
         {task-id-2 :tasks/id} (factories/create-task organization)]
-    (reset! test-context
-            {:user-1-id user-1-id
-             :organization organization
-             :organization-id organization-id
-             :user-2-id user-2-id
-             :task-id-1 task-id-1
-             :task-id-2 task-id-2})))
-
-(defn- setup-org-users-tasks-fixture
-  [f]
-  (setup-org-users-tasks)
-  (f))
+    {:user-1-id user-1-id
+     :organization organization
+     :organization-id organization-id
+     :user-2-id user-2-id
+     :task-id-1 task-id-1
+     :task-id-2 task-id-2}))
 
 (use-fixtures :once fixtures/config fixtures/datasource)
-(use-fixtures :each
-  fixtures/clear-db
-  setup-org-users-tasks-fixture)
+(use-fixtures :each fixtures/clear-db)
 
 (deftest create-timer-test
-  (testing "when a timer is created without a note"
-    (let [response (handler-timer/create {:body {:task-id (:task-id-1 @test-context)}
-                                          :user {:users/id (:user-2-id @test-context)}
-                                          :organization (:organization @test-context)})]
-      (is (= 200 (:status response))
-          "the request succeeds")
-      (is (s/valid? :timers/timer
-                    (:body response))
-          "the body is a valid timer object")
-      (is (= ""
-             (:timers/note (:body response)))
-          "the note is an empty string.")))
+  (let [test-context (setup-org-users-tasks!)]
+    (testing "when a timer is created without a note"
+      (let [response (handler-timer/create {:body {:task-id (:task-id-1 test-context)}
+                                            :user {:users/id (:user-2-id test-context)}
+                                            :organization (:organization test-context)})]
+        (is (= 200 (:status response))
+            "the request succeeds")
+        (is (s/valid? :timers/timer
+                      (:body response))
+            "the body is a valid timer object")
+        (is (= ""
+               (:timers/note (:body response)))
+            "the note is an empty string.")))
 
-  (testing "when a timer is created with a note"
-    (let [note "A sample note."
-          response (handler-timer/create {:body {:task-id (:task-id-1 @test-context)
-                                                 :note note}
-                                          :user {:users/id (:user-2-id @test-context)}
-                                          :organization (:organization @test-context)})]
-      (is (= 200 (:status response))
-          "the request succeeds")
-      (is (s/valid? :timers/timer
-                    (:body response))
-          "the body is a valid timer object")
-      (is (= note
-             (:timers/note (:body response)))
-          "the timer contains the note we added")))
+    (testing "when a timer is created with a note"
+      (let [note "A sample note."
+            response (handler-timer/create {:body {:task-id (:task-id-1 test-context)
+                                                   :note note}
+                                            :user {:users/id (:user-2-id test-context)}
+                                            :organization (:organization test-context)})]
+        (is (= 200 (:status response))
+            "the request succeeds")
+        (is (s/valid? :timers/timer
+                      (:body response))
+            "the body is a valid timer object")
+        (is (= note
+               (:timers/note (:body response)))
+            "the timer contains the note we added")))
 
-  (testing "when a timer is created with an invalid note and/or invalid task id"
-    (is (= {:status 400
-            :headers {}
-            :body {:error "Invalid Task ID or Note."}}
-           (handler-timer/create {:body {:task-id "foo"
-                                         :note 42}
-                                  :user {:users/id (:user-2-id @test-context)}
-                                  :organization (:organization @test-context)}))
-        "the request fails with an HTTP error."))
+    (testing "when a timer is created with an invalid note and/or invalid task id"
+      (is (= {:status 400
+              :headers {}
+              :body {:error "Invalid Task ID or Note."}}
+             (handler-timer/create {:body {:task-id "foo"
+                                           :note 42}
+                                    :user {:users/id (:user-2-id test-context)}
+                                    :organization (:organization test-context)}))
+          "the request fails with an HTTP error."))
 
-  (testing "when a timer is created with a non-existent task id"
-    (is (= {:status 400
-            :headers {}
-            :body {:error "Task does not exist."}}
-           (handler-timer/create {:body {:task-id Long/MAX_VALUE
-                                         :note "A valid note."}
-                                  :user {:users/id (:user-2-id @test-context)}
-                                  :organization (:organization @test-context)}))
-        "the request fails with an HTTP error."))
+    (testing "when a timer is created with a non-existent task id"
+      (is (= {:status 400
+              :headers {}
+              :body {:error "Task does not exist."}}
+             (handler-timer/create {:body {:task-id Long/MAX_VALUE
+                                           :note "A valid note."}
+                                    :user {:users/id (:user-2-id test-context)}
+                                    :organization (:organization test-context)}))
+          "the request fails with an HTTP error."))
 
-  (testing "when a user does not belong to the organization in the request"
-    (is (= {:status 403
-            :headers {}
-            :body {:error "Forbidden."}}
-           (handler-timer/create {:body {:task-id Long/MAX_VALUE
-                                         :note "A valid note."}
-                                  :user {:users/id (:user-2-id @test-context)}
-                                  :organization (factories/create-organization
-                                                 (:users/id
-                                                  (factories/create-user)))}))
-        "the request fails with an HTTP error.")))
+    (testing "when a user does not belong to the organization in the request"
+      (is (= {:status 403
+              :headers {}
+              :body {:error "Forbidden."}}
+             (handler-timer/create {:body {:task-id Long/MAX_VALUE
+                                           :note "A valid note."}
+                                    :user {:users/id (:user-2-id test-context)}
+                                    :organization (factories/create-organization
+                                                   (:users/id
+                                                    (factories/create-user)))}))
+          "the request fails with an HTTP error."))))
 
 (deftest delete-timer-test
   (testing "deleting a timer"
-    (let [{timer-id :timers/id :as timer} (factories/create-timer
-                                           (:organization-id @test-context)
-                                           (:user-2-id @test-context)
-                                           (:task-id-1 @test-context))
+    (let [test-context (setup-org-users-tasks!)
+          {timer-id :timers/id :as timer} (factories/create-timer
+                                           (:organization-id test-context)
+                                           (:user-2-id test-context)
+                                           (:task-id-1 test-context))
           response (handler-timer/delete {:params {:timer-id (str timer-id)}
-                                          :user {:users/id (:user-2-id @test-context)}})]
+                                          :user {:users/id (:user-2-id test-context)}})]
       (is (= 200
              (:status response))
           "the request succeeds")
@@ -124,24 +115,25 @@
               :headers {},
               :body {:error "Timer does not exist."}}
              (handler-timer/delete {:params {:timer-id (str timer-id)}
-                                    :user {:users/id (:user-2-id @test-context)}}))
+                                    :user {:users/id (:user-2-id test-context)}}))
           "fails with HTTP error when the timer is already deleted.")
       (is (= {:status 400
               :headers {}
               :body {:error "Invalid Timer ID."}}
              (handler-timer/delete {:params {:timer-id "42"}
-                                    :user {:users/id (:user-2-id @test-context)}}))
+                                    :user {:users/id (:user-2-id test-context)}}))
           "fails with HTTP error when the timer id is invalid"))))
 
 (deftest update-timer-note-test
   (testing "updating a timer's note"
-    (let [{timer-id :timers/id} (factories/create-timer
-                                 (:organization-id @test-context)
-                                 (:user-2-id @test-context)
-                                 (:task-id-1 @test-context))
+    (let [test-context (setup-org-users-tasks!)
+          {timer-id :timers/id} (factories/create-timer
+                                 (:organization-id test-context)
+                                 (:user-2-id test-context)
+                                 (:task-id-1 test-context))
           response (handler-timer/update-note {:params {:timer-id (str timer-id)}
                                                :body {:note "An updated note."}
-                                               :user {:users/id (:user-2-id @test-context)}})]
+                                               :user {:users/id (:user-2-id test-context)}})]
       (is (= 200
              (:status response))
           "the request succeeds")
@@ -156,24 +148,25 @@
               :body {:error "Timer does not exist."}}
              (handler-timer/update-note {:params {:timer-id (str (java.util.UUID/randomUUID))}
                                          :body {:note "An updated note."}
-                                         :user {:users/id (:user-2-id @test-context)}}))
+                                         :user {:users/id (:user-2-id test-context)}}))
           "fails with HTTP error when the timer does not exist")
       (is (= {:status 400
               :headers {}
               :body {:error "Invalid Timer ID or Note."}}
              (handler-timer/update-note {:params {:timer-id "42"}
                                          :body {:note nil}
-                                         :user {:users/id (:user-2-id @test-context)}}))
+                                         :user {:users/id (:user-2-id test-context)}}))
           "fails with HTTP error when the timer id and/or note are invalid"))))
 
 (deftest start-timer-test
   (testing "starting a timer"
-    (let [{timer-id :timers/id} (factories/create-timer
-                                 (:organization-id @test-context)
-                                 (:user-2-id @test-context)
-                                 (:task-id-1 @test-context))
+    (let [test-context (setup-org-users-tasks!)
+          {timer-id :timers/id} (factories/create-timer
+                                 (:organization-id test-context)
+                                 (:user-2-id test-context)
+                                 (:task-id-1 test-context))
           response (handler-timer/start {:params {:timer-id (str timer-id)}
-                                         :user {:users/id (:user-2-id @test-context)}})]
+                                         :user {:users/id (:user-2-id test-context)}})]
       (is (= 200
              (:status response))
           "the request succeeds")
@@ -184,28 +177,29 @@
               :headers {},
               :body {:error "Timer is already started."}}
              (handler-timer/start {:params {:timer-id (str timer-id)}
-                                   :user {:users/id (:user-2-id @test-context)}}))
+                                   :user {:users/id (:user-2-id test-context)}}))
           "fails with HTTP error when the timer is already started.")
       (is (= {:status 400
               :headers {}
               :body {:error "Timer does not exist."}}
              (handler-timer/start {:params {:timer-id (str (java.util.UUID/randomUUID))}
-                                   :user {:users/id (:user-2-id @test-context)}}))
+                                   :user {:users/id (:user-2-id test-context)}}))
           "fails with HTTP error when the timer does not exist")
       (is (= {:status 400
               :headers {}
               :body {:error "Invalid Timer ID."}}
              (handler-timer/start {:params {:timer-id "42"}
-                                   :user {:users/id (:user-2-id @test-context)}}))
+                                   :user {:users/id (:user-2-id test-context)}}))
           "fails with HTTP error when the timer id is invalid"))))
 
 (deftest stop-timer-test
   (testing "stopping a timer"
-    (let [user-2-id (:user-2-id @test-context)
+    (let [test-context (setup-org-users-tasks!)
+          user-2-id (:user-2-id test-context)
           {timer-id :timers/id} (factories/create-timer
-                                 (:organization-id @test-context)
-                                 (:user-2-id @test-context)
-                                 (:task-id-1 @test-context))
+                                 (:organization-id test-context)
+                                 (:user-2-id test-context)
+                                 (:task-id-1 test-context))
           _ (timer/start! db/datasource user-2-id timer-id)
           response (handler-timer/stop {:params {:timer-id (str timer-id)}
                                         :user {:users/id user-2-id}})]
@@ -219,7 +213,7 @@
               :headers {},
               :body {:error "Timer is already stopped."}}
              (handler-timer/stop {:params {:timer-id (str timer-id)}
-                                  :user {:users/id (:user-2-id @test-context)}}))
+                                  :user {:users/id (:user-2-id test-context)}}))
           "fails with HTTP error when the timer is already stopped.")
       (is (= {:status 400
               :headers {}
@@ -234,11 +228,12 @@
 
 (deftest find-timers-for-user-and-task-test
   (testing "when a timer is looked up by user id"
-    (let [user-2-id (:user-2-id @test-context)
-          task-id (:task-id-1 @test-context)
+    (let [test-context (setup-org-users-tasks!)
+          user-2-id (:user-2-id test-context)
+          task-id (:task-id-1 test-context)
           _timers (dotimes [_ 3]
                     (->> (timer/create! db/datasource
-                                        (:organization-id @test-context)
+                                        (:organization-id test-context)
                                         user-2-id
                                         task-id
                                         "A lovely timer.")
