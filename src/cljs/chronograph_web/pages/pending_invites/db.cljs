b99b93da8dbac3bfe693f14dd3edecfcd5174a80
Sandilya Jandhyala <sandy@nilenso.com>
2020-10-06 13:33:33 +0530

    Call APIs to fetch, accept and reject invites

--- src/cljs/chronograph_web/pages/pending_invites/db.cljs
+++ src/cljs/chronograph_web/pages/pending_invites/db.cljs
@@ -1,8 +1,13 @@
-(ns chronograph-web.pages.pending-invites.db)
+(ns chronograph-web.pages.pending-invites.db
+  (:require [chronograph-web.db :as db]))
 
 (defn invite-by-id
   [db id]
   (get-in db [:organization-invites id]))
+
+(defn slug-by-id
+  [db id]
+  (:slug (invite-by-id db id)))
 
 (defn invites
   [db]
@@ -13,8 +18,6 @@
   (update db :organization-invites (fn [invite]
                                      (dissoc invite id))))
 
-(defn set-invites-in-db
-  [db]
-  (assoc db :organization-invites {1 {:id 1 :slug "slug1" :name "org1"}
-                                   2 {:id 2 :slug "slug2" :name "org2"}
-                                   3 {:id 3 :slug "slug3" :name "org3"}}))
+(defn add-invited-orgs
+  [db invited-orgs]
+  (update db :organization-invites merge (db/normalize-by :id invited-orgs)))
--- src/cljs/chronograph_web/pages/pending_invites/events.cljs
+++ src/cljs/chronograph_web/pages/pending_invites/events.cljs
@@ -1,19 +1,61 @@
 (ns chronograph-web.pages.pending-invites.events
   (:require [re-frame.core :as rf]
-            [chronograph-web.pages.pending-invites.db :as db]))
+            [chronograph-web.pages.pending-invites.db :as pending-invites-db]
+            [chronograph-web.http :as http]
+            [chronograph-web.db :as db]))
+
+(rf/reg-event-fx
+  ::reject-invite
+  (fn [{:keys [db]} [_ id]]
+    {:http-xhrio (http/delete (str "/api/organizations/invited/" (pending-invites-db/slug-by-id db id))
+                              {:on-success [::reject-invite-succeeded id]
+                               :on-failure [::reject-invite-failed]})}))
 
 (rf/reg-event-db
-  ::reject-invite
+  ::reject-invite-failed
+  (fn [db _]
+    (db/report-error db ::error-reject-invite-failed)))
+
+(rf/reg-event-db
+  ::reject-invite-succeeded
   (fn [db [_ id]]
-    (db/remove-invite db id)))
+    (-> db
+        (db/remove-error ::error-reject-invite-failed)
+        (pending-invites-db/remove-invite id))))
 
 (rf/reg-event-fx
   ::accept-invite
   (fn [{:keys [db]} [_ id]]
-    {:db            (db/remove-invite db id)
-     :history-token (str "/organization/" (:slug (db/invite-by-id db id)))}))
+    {:http-xhrio (http/post (str "/api/organizations/invited/" (pending-invites-db/slug-by-id db id))
+                            {:on-success [::accept-invite-succeeded id]
+                             :on-failure [::accept-invite-failed]})}))
+
+(rf/reg-event-fx
+  ::accept-invite-succeeded
+  (fn [{:keys [db]} [_ id]]
+    {:db            (-> db
+                        (db/remove-error ::error-accept-invite-failed)
+                        (pending-invites-db/remove-invite id))
+     :history-token (str "/organization/" (:slug (pending-invites-db/invite-by-id db id)))}))
 
 (rf/reg-event-db
-  ::page-mounted
+  ::accept-invite-failed
   (fn [db _]
-    (db/set-invites-in-db db)))
+    (db/report-error db ::error-accept-invite-failed)))
+
+(rf/reg-event-fx
+  ::fetch-invited-orgs
+  (fn [_ _]
+    {:http-xhrio (http/get "/api/organizations/invited"
+                           {:on-success [::fetch-invited-orgs-success]
+                            :on-failure [::fetch-invited-orgs-failed]})}))
+
+(rf/reg-event-db
+  ::fetch-invited-orgs-success
+  (fn [db [_ invited-orgs]]
+    (pending-invites-db/add-invited-orgs db invited-orgs)))
+
+(rf/reg-event-db
+  ::fetch-invited-orgs-failed
+  (fn [db _]
+    (db/report-error db ::error-fetch-invited-orgs-failed)))
