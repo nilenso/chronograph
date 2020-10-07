5f4317af4c1ec7f9d9f1d074894c632cc2c14b10
Govind krishna Joshi <govind@nilenso.com>
2020-10-07 12:12:28 +0530

    Use db core functions for users

--- src/clj/chronograph/domain/user.clj
+++ src/clj/chronograph/domain/user.clj
@@ -4,7 +4,8 @@
 
 (def find-by-google-id user-db/find-by-google-id)
 
-(def find-by-id user-db/find-by-id)
+(defn find-by-id [tx user-id]
+  (user-db/find-by tx {:users/id user-id}))
 
 (defn find-or-create-google-user!
   [tx google-id name email photo-url]
@@ -12,4 +13,8 @@
     google-user
     (let [{google-profiles-id :google-profiles/id}
           (google-profile-db/create! tx {:google-profiles/google-id google-id})]
-      (user-db/create! tx name email photo-url google-profiles-id))))
+      (user-db/create! tx
+                       {:users/name name
+                        :users/email  email
+                        :users/photo-url photo-url
+                        :users/google-profiles-id google-profiles-id}))))
