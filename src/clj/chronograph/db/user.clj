268fd716f3ae34e11bbe332992366f22087357d5
Govind krishna Joshi <govind@nilenso.com>
2020-10-07 12:12:28 +0530

    Use db core functions for users

--- src/clj/chronograph/db/user.clj
+++ src/clj/chronograph/db/user.clj
@@ -1,30 +1,28 @@
 (ns chronograph.db.user
   (:require [next.jdbc.sql :as sql]
-            [chronograph.db.core :as db]
-            [chronograph.utils.time :as time]))
+            [chronograph.db.core :as db]))
 
-(defn create! [tx name email photo-url google-profiles-id]
-  (let [now (time/now)]
-    (sql/insert! tx
-                 :users
-                 {:name               name
-                  :email              email
-                  :photo-url          photo-url
-                  :google-profiles-id google-profiles-id
-                  :created-at         now
-                  :updated-at         now}
-                 db/sql-opts)))
+(defn create! [tx {:users/keys [name
+                                email
+                                photo-url
+                                google-profiles-id]}]
+  (db/create! :users
+              tx
+              {:users/name name
+               :users/email email
+               :users/photo-url photo-url
+               :users/google-profiles-id google-profiles-id}))
+
+(defn find-by [tx attributes]
+  (db/find-by :users
+              tx
+              attributes))
 
 (defn find-by-google-id [tx google-id]
   (first (sql/query tx
                     ["SELECT u.* FROM users u
                      INNER JOIN google_profiles gp ON u.google_profiles_id=gp.id
                      AND gp.google_id=?" google-id]
-                    db/sql-opts)))
-
-(defn find-by-id [tx user-id]
-  (first (sql/query tx
-                    ["SELECT * FROM users WHERE id=?" user-id]
                     db/sql-opts)))
 
 (defn find-by-org-id
