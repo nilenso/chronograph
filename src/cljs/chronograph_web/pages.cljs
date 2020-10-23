f2809b168253f8ba110e9fb68e8f96c18495bebc
Sandilya Jandhyala <sandy@nilenso.com>
2020-10-23 18:14:55 +0530

    Fix alignment issues in the header and wrap some more antd components

--- src/cljs/chronograph_web/pages.cljs
+++ src/cljs/chronograph_web/pages.cljs
@@ -3,61 +3,64 @@
             [reagent.core :as r]
             [chronograph-web.events.organization :as org-events]
             [chronograph-web.subscriptions :as subs]
-            ["antd" :as antd]
+            ["antd" :as antd-raw]
+            [chronograph-web.components.antd :as antd]
             ["@ant-design/icons" :as icons]))
 
 (defn- header-logo []
   [:> icons/ClockCircleOutlined {:style {:color "wheat" :fontSize "32px"}}])
 
 (defn- user-menu []
-  [:> antd/Menu {:mode "inline"}
-   [:> antd/Menu.Item {:key "1" :icon (r/create-element icons/SettingOutlined)} "Settings"]
-   [:> antd/Menu.Divider {:key "2"}]
-   [:> antd/Menu.Item {:key "3" :icon (r/create-element icons/LogoutOutlined)} "Logout"]])
+  [antd/menu {:mode "inline"}
+   (antd/menu-item {:key "1" :icon (r/create-element icons/SettingOutlined)} "Settings")
+   (antd/menu-divider {:key "2"})
+   (antd/menu-item {:key "3" :icon (r/create-element icons/LogoutOutlined)} "Logout")])
 
-(defn- user-dropdown []
-  [:> antd/Dropdown {:overlay (r/create-element (r/reactify-component user-menu))}
+(defn- user-dropdown [name photo-url]
+  [antd/dropdown {:overlay (r/as-element [user-menu])}
    [:span {:style {:color "wheat"}}
-    [:> antd/Avatar {:size "large"
-                     :src "profile.png"}]
-    "Name"]])
+    [antd/space
+     [antd/avatar {:size "large"
+                   :src  photo-url}]
+     name]]])
 
-(defn- user-header [{:keys [name]}]
-  [:> antd/Layout.Header
-   [:> antd/Row {:justify "space-between"}
+(defn- user-header [{:keys [name photo-url]}]
+  [antd/layout-header
+   [antd/row {:justify "space-between"
+              :align   "middle"}
     [header-logo]
-    [user-dropdown]]])
+    [user-dropdown name photo-url]]])
 
 (defn with-user-header []
   (fn [_]
-    (let [children (r/children (r/current-component))
+    (let [children  (r/children (r/current-component))
           user-info @(rf/subscribe [::subs/user-info])]
-      [:> antd/Layout
+      [antd/layout
        [user-header user-info]
-       (into [:> antd/Layout.Content {:style {:padding "0 50px"}}] children)])))
+       (into [antd/layout-content {:style {:padding "0 50px"}}] children)])))
 
 ;; Unused code. But, should be useful soon.
 (defn- organization-select-menu []
-  [:> antd/Menu {:mode "inline"}
-   [:> antd/Menu.Item {:key "1"} "nilenso"]
-   [:> antd/Menu.Item {:key "2"} "client"]
-   [:> antd/Menu.Divider {:key "3"}]
-   [:> antd/Menu.Item {:key "4" :icon (r/create-element icons/PlusOutlined)} "New"]])
+  [:> antd-raw/Menu {:mode "inline"}
+   [:> antd-raw/Menu.Item {:key "1"} "nilenso"]
+   [:> antd-raw/Menu.Item {:key "2"} "client"]
+   [:> antd-raw/Menu.Divider {:key "3"}]
+   [:> antd-raw/Menu.Item {:key "4" :icon (r/create-element icons/PlusOutlined)} "New"]])
 
 (defn- organization-sider []
-  [:> antd/Layout.Sider
-   [:> antd/Menu {:mode "inline"}
-    [:> antd/Menu.Item {:key "1" :icon (r/create-element icons/ClockCircleOutlined)} "Timers"]
-    [:> antd/Menu.SubMenu {:title "Admin" :icon (r/create-element icons/TeamOutlined) :key "2"}
-     [:> antd/Menu.Item {:key "2.1"} "Members"]
-     [:> antd/Menu.Item {:key "2.2"} "Tasks"]]]])
+  [:> antd-raw/Layout.Sider
+   [:> antd-raw/Menu {:mode "inline"}
+    [:> antd-raw/Menu.Item {:key "1" :icon (r/create-element icons/ClockCircleOutlined)} "Timers"]
+    [:> antd-raw/Menu.SubMenu {:title "Admin" :icon (r/create-element icons/TeamOutlined) :key "2"}
+     [:> antd-raw/Menu.Item {:key "2.1"} "Members"]
+     [:> antd-raw/Menu.Item {:key "2.2"} "Tasks"]]]])
 
 (defn- user-and-organization-header []
-  [:> antd/Layout.Header
-   [:> antd/Row {:justify "space-between"}
+  [:> antd-raw/Layout.Header
+   [:> antd-raw/Row {:justify "space-between"}
     [header-logo]
-    [:> antd/Dropdown {:trigger "click"
-                       :overlay (r/create-element (r/reactify-component organization-select-menu))}
+    [:> antd-raw/Dropdown {:trigger "click"
+                           :overlay (r/create-element (r/reactify-component organization-select-menu))}
      [:> icons/DownOutlined {:style {:color "wheat" :fontSize "32px"}}]]]])
 
 (defn with-organization-sidebar [_]
@@ -65,8 +68,8 @@
   (fn [_]
     (let [children (r/children (r/current-component))]
       [:<>
-       [:> antd/Layout
+       [:> antd-raw/Layout
         [user-and-organization-header]
-        [:> antd/Layout
+        [:> antd-raw/Layout
          [organization-sider]
-         (into [:> antd/Layout.Content] children)]]])))
+         (into [:> antd-raw/Layout.Content] children)]]])))
