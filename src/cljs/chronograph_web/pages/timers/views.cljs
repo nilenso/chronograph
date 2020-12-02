f7daecf83fc9c7b3fb4482a5d6061dddcae64bec
Sandilya Jandhyala <sandy@nilenso.com>
2020-12-02 18:44:49 +0530

    Refactor and break down the timer display components

--- src/cljs/chronograph_web/pages/timers/views.cljs
+++ src/cljs/chronograph_web/pages/timers/views.cljs
@@ -16,19 +16,27 @@
             [clojure.string :as string]
             [cljs.spec.alpha :as s]))
 
-(defn- duration-display [{:keys [hours minutes]} show-colon?]
-  [antd/row {:wrap  false
-             :class "timer-duration large-text"}
-   [antd/text {:strong "true"} hours]
-   [antd/text {:class  (str "timer-duration-colon "
-                            (when-not show-colon?
-                              "invisible"))
-               :strong "true"} ":"]
-   [antd/text {:strong "true"} (ustring/left-pad 2 "0" (str minutes))]])
-
 (defn- running? [{:keys [time-spans]}]
   (and (not-empty time-spans)
        (not (:stopped-at (last time-spans)))))
+
+(defn- duration-display [timer]
+  (r/with-let [current-time            (r/atom (js/Date.))
+               show-colon?              (r/atom true)
+               timer-interval-id       (js/setInterval #(reset! current-time (js/Date.)) 1000)
+               colon-blink-interval-id (js/setInterval #(swap! show-colon? not) 500)]
+    (let [{:keys [hours minutes]} (time/timer-duration timer @current-time)]
+      [antd/row {:wrap  false
+                 :class "timer-duration large-text"}
+       [antd/text {:strong "true"} hours]
+       [antd/text {:class  (str "timer-duration-colon "
+                                (when (and (running? timer)
+                                           (not @show-colon?))
+                                  "invisible"))
+                   :strong "true"} ":"]
+       [antd/text {:strong "true"} (ustring/left-pad 2 "0" (str minutes))]])
+    (finally (js/clearInterval timer-interval-id)
+             (js/clearInterval colon-blink-interval-id))))
 
 (defn- task-option [{:keys [id name]}]
   (with-meta (antd/option {:value id :key id} name)
@@ -90,66 +98,71 @@
        [antd/input (assoc (get-input-attributes :duration-in-hour-mins)
                           :placeholder "Time duration for eg. 1:26")]]]]))
 
+(defn- start-stop-button [{:keys [id] :as timer}]
+  (if (running? timer)
+    [antd/button {:type    "primary"
+                  :icon    icons/PauseCircleFilled
+                  :onClick #(rf/dispatch [::timers-events/stop-timer id])
+                  :style   {:min-width "86px"}}
+     "Stop"]
+    [antd/button {:icon    icons/PlayCircleFilled
+                  :onClick #(rf/dispatch [::timers-events/start-timer id])
+                  :style   {:min-width "86px"}}
+     "Start"]))
+
+(defn- delete-button [timer-id]
+  (r/with-let [show-delete-popconfirm? (r/atom false)]
+    [antd/popconfirm {:title           "Are you sure you want to delete this timer?"
+                      :visible         @show-delete-popconfirm?
+                      :onVisibleChange #(reset! show-delete-popconfirm? %)
+                      :okText          "Yes"
+                      :cancelText      "No"
+                      :placement       "bottom"
+                      :onConfirm       #(rf/dispatch [::timers-events/delete-timer timer-id])}
+     [antd/button {:type    "danger"
+                   :icon    icons/DeleteOutlined
+                   :onClick #(reset! show-delete-popconfirm? true)}]]))
+
+(defn- task-name-row [task-name]
+  [antd/row [antd/text {:class  "large-text"
+                        :strong "true"} task-name]])
+
+(defn- note-row [note]
+  (when (and note
+             (not= "" note))
+    [antd/row [antd/text {:type "secondary"} note]]))
+
 (defn timer-display [{:keys [task note id] :as timer}]
-  (r/with-let [current-time            (r/atom (js/Date.))
-               show-colon              (r/atom true)
-               timer-interval-id       (js/setInterval #(reset! current-time (js/Date.)) 1000)
-               colon-blink-interval-id (js/setInterval #(swap! show-colon not) 500)
-               show-delete-popconfirm? (r/atom false)]
-    [:<>
-     [antd/row {:class  (str "timer " (when (running? timer)
-                                        "timer-running"))
-                :align  "middle"
-                :wrap   false
-                :gutter 16}
-      [antd/col
-       [duration-display
-        (time/timer-duration timer @current-time)
-        (or (not (running? timer))
-            @show-colon)]]
+  [:<>
+   [antd/row {:class  (str "timer " (when (running? timer)
+                                      "timer-running"))
+              :align  "middle"
+              :wrap   false
+              :gutter 16}
+    [antd/col
+     [duration-display timer]]
+    [antd/col
+     [task-name-row (:name task)]
+     [note-row note]
+     [antd/row
+      [antd/space
+       [start-stop-button timer]
+       [edit-timer-button-and-form id]
+       [delete-button id]]]]]])
 
-      [antd/col
-       [antd/row [antd/text {:class  "large-text"
-                             :strong "true"} (:name task)]]
-       (when (and note
-                  (not= "" note))
-         [antd/row [antd/text {:type "secondary"} note]])
-       [antd/row
-        [antd/space
-         (if (running? timer)
-           [antd/button {:type    "primary"
-                         :icon    icons/PauseCircleFilled
-                         :onClick #(rf/dispatch [::timers-events/stop-timer id])}
-            "Stop"]
-           [antd/button {:icon    icons/PlayCircleFilled
-                         :onClick #(rf/dispatch [::timers-events/start-timer id])}
-            "Start"])
-         [edit-timer-button-and-form
-          id]
-         [antd/popconfirm {:title           "Are you sure you want to delete this timer?"
-                           :visible         @show-delete-popconfirm?
-                           :onVisibleChange #(reset! show-delete-popconfirm? %)
-                           :okText          "Yes"
-                           :cancelText      "No"
-                           :placement       "bottom"
-                           :onConfirm       #(rf/dispatch [::timers-events/delete-timer id])}
-          [antd/button {:type    "danger"
-                        :icon    icons/DeleteOutlined
-                        :onClick #(reset! show-delete-popconfirm? true)}]]]]]]]
-    (finally (js/clearInterval timer-interval-id)
-             (js/clearInterval colon-blink-interval-id))))
-
-(defn create-timer-widget [on-cancel-event create-timer-succeeded-event create-timer-failed-event]
+(defn create-timer-widget []
   (r/with-let [{::form/keys [get-submit-attributes
                              get-input-attributes
-                             get-select-attributes]} (form/form {:form-key        ::create-timer-form
-                                                                 :request-builder (fn [{:keys [task note]}]
-                                                                                    (api/create-and-start-timer
-                                                                                     task
-                                                                                     note
-                                                                                     (time/current-calendar-date)
-                                                                                     create-timer-succeeded-event
-                                                                                     create-timer-failed-event))})]
+                             get-select-attributes]}
+               (form/form {:form-key        ::create-timer-form
+                           :specs           {:task :tasks/id}
+                           :request-builder (fn [{:keys [task note]}]
+                                              (api/create-and-start-timer
+                                               task
+                                               note
+                                               (time/current-calendar-date)
+                                               [::timers-events/create-timer-succeeded]
+                                               [::timers-events/create-timer-failed]))})]
     [antd/space {:direction "vertical"
                  :class     "timer create-timer-widget"}
      [task-chooser (get-select-attributes :task)]
@@ -160,7 +173,7 @@
       [antd/button (assoc (get-submit-attributes) :icon icons/PlayCircleFilled)
        "Start"]
       [antd/button
-       {:onClick #(rf/dispatch on-cancel-event)}
+       {:onClick #(rf/dispatch [::timers-events/dismiss-create-timer-widget])}
        "Cancel"]]]))
 
 (defn timer-list [ds]
@@ -169,10 +182,7 @@
               :renderItem (fn [{:keys [id state] :as timer}]
                             (if (= "creating" state)
                               [antd/list-item {:key "creating"}
-                               [create-timer-widget
-                                [::timers-events/dismiss-create-timer-widget]
-                                [::timers-events/create-timer-succeeded]
-                                [::timers-events/create-timer-failed]]]
+                               [create-timer-widget]]
                               [antd/list-item {:key   id
                                                :style {:height "100%"}}
                                [timer-display
